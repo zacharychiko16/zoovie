34f8f9e7fd20dc695ceb2b8defdad3724d268b1a
zacharychiko16 <zacharychiko16@gmail.com>
2023-05-02 16:09:52 -0400

    improving Cleanliness

--- app.js
+++ app.js
@@ -1,108 +1,3 @@
-// const API_URL =
-//   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=539565c7581c473ffa0b2f312a86eb8a&page=1";
-// const SEARCH_URL =
-//   'https://api.themoviedb.org/3/search/movie?api_key=539565c7581c473ffa0b2f312a86eb8a&query=';
-// const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280/";
-
-// const form = document.getElementById("form");
-// const search = document.getElementById("search");
-// const main = document.getElementById("main");
-// const sortByDateBtn = document.getElementById("sort-by-date");
-// const sortByRatingBtn = document.getElementById("sort-by-rating");
-
-// let movies = []; // declare movies array to store data from API
-
-// getMovies(API_URL);
-// async function getMovies(url) {
-//   const res = await fetch(url);
-//   const data = await res.json();
-//   displayMovies(data.results)
-//   console.log(data.results);
-// }
-
-// function displayMovies(movies) {
-//     main.innerHTML = "";
-//     for (let i = 0; i <= 5  && i < movies.length; i++) {
-//       const { title, poster_path, vote_average, overview } = movies[i];
-//       const moviesElement = document.createElement("div");
-//       moviesElement.classList.add("movie");
-//       moviesElement.innerHTML = `
-//         <img src="${IMAGE_PATH + poster_path}" alt="${title}" />
-//         <div class="movie-info">
-//           <h3>${title}</h3>
-//           <span class="${getClassesByRating(vote_average)}"> ${vote_average}</span>
-//           <div class="overview">
-//             <h3>Overview</h3>
-//             ${overview} 
-//           </div>
-//         </div>`;
-  
-//       main.appendChild(moviesElement);
-//     }
-//   }
-
-//   sortByDateBtn.addEventListener("click", () => {
-//     const sortedMovies = sortMoviesByDate(movies);
-//     displayMovies(sortedMovies);
-//   });
-  
-//   sortByRatingBtn.addEventListener("click", () => {
-//     const sortedMovies = sortMoviesByRating(movies);
-//     displayMovies(sortedMovies);
-//   });
-
-//   const sortRadios = document.querySelectorAll(
-//     'input[type="radio"][name="sort-type"]'
-//   );
-
-//   sortRadios.forEach((radio) => {
-//     radio.addEventListener("change", () => {
-//       const sortType = document.querySelector(
-//         'input[type="radio"][name="sort-type"]:checked'
-//       ).value;
-//       if (sortType === "date") {
-//         const sortedMovies = sortMoviesByDate(movies);
-//         displayMovies(sortedMovies);
-//       } else if (sortType === "rating") {
-//         const sortedMovies = sortMoviesByRating(movies);
-//         displayMovies(sortedMovies);
-//       }
-//     });
-//   });
-
-//   function sortMoviesByDate(movies) {
-//     return movies.sort(
-//       (a, b) => new Date(b.release_date) - new Date(a.release_date)
-//     );
-//   }
-  
-  
-//   function sortMoviesByRating(movies) {
-//     return movies.sort((a, b) => b.vote_average - a.vote_average);
-//   }
-  
-  
-
-// function getClassesByRating(rating) {
-//     if(rating>=8){
-//         return 'green'
-//     }else if(rating>=5){
-//         return 'orange'
-//     }else {
-//         return
-//     }
-// }
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//    const searchValue = search.value;
-//   if (searchValue && searchValue !== "") {
-//     getMovies(SEARCH_URL+searchValue);
-//     searchValue = "";
-//   } else {
-//     window.location.reload();
-//   }
-// });
-
 const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=539565c7581c473ffa0b2f312a86eb8a&page=1";
 const SEARCH_URL =
@@ -112,8 +7,7 @@ const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280/";
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const main = document.getElementById("main");
-const sortByDateBtn = document.getElementById("sort-by-date");
-const sortByRatingBtn = document.getElementById("sort-by-rating");
+const filterSelect = document.getElementById("filter");
 
 let movies = []; // declare movies array to store data from API
 
@@ -148,32 +42,20 @@ function displayMovies(movies) {
   }
 }
 
-sortByDateBtn.addEventListener("click", () => {
-  const sortedMovies = sortMoviesByDate(movies);
-  displayMovies(sortedMovies);
-});
+filterSelect.addEventListener("change", () => {
+  const selectedOption = filterSelect.value;
+  let sortedMovies;
 
-sortByRatingBtn.addEventListener("click", () => {
-  const sortedMovies = sortMoviesByRating(movies);
-  displayMovies(sortedMovies);
-});
+  if (selectedOption === "By Release Date ") {
+    sortedMovies = sortMoviesByDate(movies);
+  } else if (selectedOption === "By Rating") {
+    sortedMovies = sortMoviesByRating(movies);
+  } else {
+    // Handle Default case here
+    sortedMovies = movies;
+  }
 
-const sortRadios = document.querySelectorAll(
-  'input[type="radio"][name="sort-type"]'
-);
-sortRadios.forEach((radio) => {
-  radio.addEventListener("change", () => {
-    const sortType = document.querySelector(
-      'input[type="radio"][name="sort-type"]:checked'
-    ).value;
-    if (sortType === "date") {
-      const sortedMovies = sortMoviesByDate(movies);
-      displayMovies(sortedMovies);
-    } else if (sortType === "rating") {
-      const sortedMovies = sortMoviesByRating(movies);
-      displayMovies(sortedMovies);
-    }
-  });
+  displayMovies(sortedMovies);
 });
 
 function sortMoviesByDate(movies) {
@@ -195,7 +77,7 @@ function getClassesByRating(rating) {
     return;
   }
 }
-form.addEventListener('submit', (e) => {
+function searchMovie(e){
     e.preventDefault();
      const searchValue = search.value;
     if (searchValue && searchValue !== "") {
@@ -204,5 +86,5 @@ form.addEventListener('submit', (e) => {
     } else {
       window.location.reload();
     }
-  });
+  };
 
